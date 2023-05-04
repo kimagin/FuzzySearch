933ca74c407089ce0d874f1e0cd9063c8d5ee83b
kimagin <info@kimagination.net>
2023-05-04 22:05:29 +0200

    Creating the search with Pagefind

--- src/js/global.js
+++ src/js/global.js
@@ -1,45 +1,7 @@
-import Fuse from 'fuse.js'
-
 // Create a global data object for Alpine
 document.addEventListener('alpine:init', () => {
   Alpine.data('data', () => ({
-    async init() {
-      // Receive all the post information retrived from the front matter via a data-attribute. We need to parse the text into a JSON object
-      const allPosts = JSON.parse(
-        document.getElementById('results').dataset.posts
-      )
-
-      // Fuse will use the following options
-      const options = {
-        includeMatches: true,
-        minMatchCharLength: 1,
-        threshold: 0.5,
-        keys: [
-          'data.title',
-          'data.shortDescription',
-          'data.category',
-          'data.author',
-        ],
-      }
-      // Create a new Fuzzy Search instance from all the posts
-      const fuse = new Fuse(allPosts, options)
-      const unfilteredPosts = allPosts.map((post) => {
-        return {
-          item: post,
-        }
-      })
-
-      this.filteredPosts = unfilteredPosts
-      // Any time that the query is changed, filteredPosts will get updated
-      this.$watch('query', (value) => {
-        if (value === '') {
-          console.log('empty')
-          this.filteredPosts = unfilteredPosts
-        } else {
-          this.filteredPosts = fuse.search(this.query)
-        }
-      })
-    },
+    async init() {},
     query: '',
     filteredPosts: [],
   }))
